4434c5985e4a0f77bda55da1b3999b07087a7072
mattemonte03 <117922070+mattemonte03@users.noreply.github.com>
2022-11-14 19:02:44 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
+
 int main() {
+float a=0,b=0,x=0,y=0;
+cin>>a>>b>>x>>y;
+cout<<"inserire valori per a,b,x,y:\n";
+if(x<0 && y>0){
+cout<<a*x-b*y;}
+else if(x>=0 && y<=0){
+cout<<a*(x*x)-b*y;}
+else{
+cout<<a*x+b*(y*y);
+}
    return 0;
 }
